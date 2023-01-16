4f8d6527693a4be3da801c29c91bd8f4f42acb7f
nikhil-solankhi <nikhil.solankhi.ns@gmail.com>
2023-01-17 03:06:12 +0530

    updated

--- app5/myapp/Controllers/EmployeeController.cs
+++ app5/myapp/Controllers/EmployeeController.cs
@@ -33,10 +33,62 @@ public class EmployeeController : Controller
     }
 
     [HttpPost]
-    public IActionResult Add(Employee empform)
+    public IActionResult Add(Employee empformadd)
     {
-        _DBTESTContext.Employees.Add(empform);
-        _DBTESTContext.SaveChangesAsync();
+        _DBTESTContext.Employees.Add(empformadd);
+        _DBTESTContext.SaveChanges();
+        return RedirectToAction("GetEmployees");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(Employee empformdel)
+    {
+        _DBTESTContext.Employees.Remove(empformdel);
+        _DBTESTContext.SaveChanges();
+        return RedirectToAction("GetEmployees");
+    }
+    [HttpGet]
+    public IActionResult Edit(Employee empformedit)
+    {
+        return RedirectToAction("EditView",empformedit);
+    }
+
+    [HttpGet]
+    public IActionResult EditView(Employee empformedit)
+    {
+        Employee emp = _DBTESTContext.Employees.Find(empformedit.Id);
+        ViewData["empedit"] = emp;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult updateEmp(Employee empUpdate)
+    {
+        var emp = _DBTESTContext.Employees.FirstOrDefault(x=>x.Id==empUpdate.Id);
+        if(emp != null){
+            emp.Name=empUpdate.Name;
+            emp.Email=empUpdate.Email;
+            emp.Address=empUpdate.Address;
+            emp.Phone=empUpdate.Phone;
+
+            
+        }
+        _DBTESTContext.SaveChanges();
+        return RedirectToAction("GetEmployees"); 
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(Employee empformadd)
+    {
+        _DBTESTContext.Employees.Add(empformadd);
+        _DBTESTContext.SaveChanges();
         return RedirectToAction("GetEmployees");
     }
 
---
+++ app5/myapp/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace myapp.Models
+{
+    public partial class LoginModel
+    {
+        
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
